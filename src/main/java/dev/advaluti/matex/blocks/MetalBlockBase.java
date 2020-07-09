dc8cc3f84f6868e4337b5762bcaf40c541434223
Michael <michaelcurtis7757@gmail.com>
2020-07-09 22:05:58 +0100

    Enabled metal blocks to be used as beacon bases and ingots as beacon fuel

--- src/main/java/dev/advaluti/matex/blocks/MetalBlockBase.java
+++ src/main/java/dev/advaluti/matex/blocks/MetalBlockBase.java
@@ -1,12 +1,13 @@
 package dev.advaluti.matex.blocks;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ToolType;
+import net.minecraftforge.common.extensions.IForgeBlock;
 
-public class MetalBlockBase extends Block {
+public class MetalBlockBase extends Block implements IForgeBlock {
 
     public MetalBlockBase() {
         super(Properties.create(Material.ROCK)
@@ -15,5 +16,11 @@ public class MetalBlockBase extends Block {
                 .harvestTool(ToolType.PICKAXE)
                 .harvestLevel(2)
         );
-}
+    }
+
+    public boolean isBeaconBase(BlockState state, IWorldReader world, BlockPos pos, BlockPos beacon)
+    {
+        return  state.getBlock() == this;
+    }
+
 }
--- src/main/java/dev/advaluti/matex/items/IngotBase.java
+++ src/main/java/dev/advaluti/matex/items/IngotBase.java
@@ -2,8 +2,11 @@ package dev.advaluti.matex.items;
 
 import dev.advaluti.matex.setup.ModSetup;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.extensions.IForgeItem;
 
-public class IngotBase extends Item {
+public class IngotBase extends Item implements IForgeItem {
 
 
     public IngotBase() {
@@ -11,5 +14,9 @@ public class IngotBase extends Item {
                 .maxStackSize(64)
                 .group(ModSetup.MATEX_GROUP)
         );
+    }
+
+    public boolean isBeaconPayment(ItemStack stack) {
+        return true;
     }
 }
